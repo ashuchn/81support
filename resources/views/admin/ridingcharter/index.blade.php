c9472c1a2b362dddb75ac92e984097e707ac34ba
Neeraj Adhav <32135561+neerajadhav@users.noreply.github.com>
2023-03-02 15:31:00 +0530

    Changes to be committed: 	modified:   resources/views/admin/ridingcharter/index.blade.php

--- resources/views/admin/ridingcharter/index.blade.php
+++ resources/views/admin/ridingcharter/index.blade.php
@@ -168,18 +168,7 @@
                 $('input[type="checkbox"]').click(function() {
                     if ($(this).prop("checked") == true) {
                         var id = $(this).prop('id');
-                        $.ajax({
-                            type: 'POST',
-                            url: "{{ route('change_status_ridingcharter') }}",
-                            data: {
-                                id: id,
-                                _token: '{{ csrf_token() }}',
-                                status: 0
-                            }
-                            success: function(data) {
-                                alert(data.success);
-                            }
-                        });
+                        alert('checked');
                     } else if ($(this).prop("checked") == false) {
                         var id = $(this).prop('id');
                         alert('unchecked');
