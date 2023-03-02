0c5b60ab18852ac484e3301ae61fb7a52937ee2c
Neeraj Adhav <32135561+neerajadhav@users.noreply.github.com>
2023-03-02 23:50:16 +0530

    modified:   resources/views/admin/category/products.blade.php

--- resources/views/admin/category/products.blade.php
+++ resources/views/admin/category/products.blade.php
@@ -6,7 +6,8 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0 text-dark">Showing Products in <b>{{ !empty($category[0]) ? $category[0] : '' }}</b> Category</h1>
+                        <h1 class="m-0 text-dark">Showing Products in <b>{{ !empty($category[0]) ? $category[0] : '' }}</b>
+                            Category</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -21,91 +22,167 @@
         <section class="content">
             <div class="container-fluid">
                 {{-- <button "></button> --}}
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Features</th>
+                            <th>Rating</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($data as $item)
+                            <tr>
+                                <td>
+                                    <div id="carouselExampleControls<?php echo $i; ?>" class="carousel slide"
+                                        data-ride="carousel">
+                                        <div class="carousel-inner">
+                                            <?php
+                                            $active = true;
+                                            ?>
+                                            @foreach ($item->images as $img)
+                                                <div class="carousel-item <?php if ($active == true) {
+                                                    echo 'active';
+                                                } ?>">
+                                                    <img class="img-fluid card-img-top img-thumbnail"
+                                                        src="{{ $img }}" style="height: 30%" alt="First slide">
+                                                    <div class="card-img-overlay">
+
+                                                    </div>
+                                                </div>
+                                                <?php $active = false; ?>
+                                            @endforeach
+
+                                        </div>
+                                        <a class="carousel-control-prev" href="#carouselExampleControls<?php echo $i; ?>"
+                                            role="button" data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselExampleControls<?php echo $i; ?>"
+                                            role="button" data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
+                                </td>
+                                <td>{{ $item->productName }}</td>
+                                <td>{{ $item->description }}</td>
+                                <td>
+                                    <a href="#" class="btn btn-light">Details</a>
+                                    <button class="btn btn-light">Edit</button>
+                                </td>
+                            </tr>
+                        @empty
+                            No record Found
+                        @endforelse
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Features</th>
+                            <th>Rating</th>
+                            <th>Actions</th>
+                        </tr>
+                    </tfoot>
+
+                </table>
+
                 <div class="row">
                     <?php $i = 1; ?>
-                      @forelse($data as $item)
-                      
+                    @forelse($data as $item)
                         <!-- <div class="col-sm-12 col-md-6 col-lg-4 col-lg-2 mb-4"> -->
                         <div class="col-sm-12  mb-4">
-                        <div class="card">
-                            <!-- <img class="card-img-top" src="" alt="Card image cap"> -->
-                            <div class="row">
+                            <div class="card">
+                                <!-- <img class="card-img-top" src="" alt="Card image cap"> -->
+                                <div class="row">
                                     <div class="col-md-4">
-                                          <div id="carouselExampleControls<?php echo $i ?>" class="carousel slide" data-ride="carousel">
-                                              <div class="carousel-inner">
+                                        <div id="carouselExampleControls<?php echo $i; ?>" class="carousel slide"
+                                            data-ride="carousel">
+                                            <div class="carousel-inner">
                                                 <?php
-                                                  $active = TRUE;
+                                                $active = true;
                                                 ?>
-                                                @foreach($item->images as $img)
-                                                    <div class="carousel-item <?php if($active == TRUE) { echo "active" ;} ?>">
-                                                      <img class="img-fluid card-img-top img-thumbnail" src="{{ $img }}" style="height: 30%" alt="First slide">
-                                                      <div class="card-img-overlay">
-                                                            
-                                                      </div>
+                                                @foreach ($item->images as $img)
+                                                    <div class="carousel-item <?php if ($active == true) {
+                                                        echo 'active';
+                                                    } ?>">
+                                                        <img class="img-fluid card-img-top img-thumbnail"
+                                                            src="{{ $img }}" style="height: 30%" alt="First slide">
+                                                        <div class="card-img-overlay">
+
+                                                        </div>
                                                     </div>
-                                                    <?php  $active = FALSE; ?>
+                                                    <?php $active = false; ?>
                                                 @endforeach
-                                                    
-                                              </div>
-                                              <a class="carousel-control-prev" href="#carouselExampleControls<?php echo $i ?>" role="button" data-slide="prev">
+
+                                            </div>
+                                            <a class="carousel-control-prev"
+                                                href="#carouselExampleControls<?php echo $i; ?>" role="button"
+                                                data-slide="prev">
                                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                                 <span class="sr-only">Previous</span>
-                                              </a>
-                                              <a class="carousel-control-next" href="#carouselExampleControls<?php echo $i ?>" role="button" data-slide="next">
+                                            </a>
+                                            <a class="carousel-control-next"
+                                                href="#carouselExampleControls<?php echo $i; ?>" role="button"
+                                                data-slide="next">
                                                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                                 <span class="sr-only">Next</span>
-                                              </a>
-                                          </div>
+                                            </a>
+                                        </div>
                                     </div>
                                     <div class="col-md-8">
                                         <div class="card-body">
-                                           
-                                              <div class="row">
-                                                  <div class="col-md-12">
-                                                      <h5 class="card-title pb-2">
+
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <h5 class="card-title pb-2">
                                                         <b>{{ $item->productName }}</b>
-                                                      </h5>
-                                                  </div>     
-                                              </div>
-                                              
-                                              
-                                            
+                                                    </h5>
+                                                </div>
+                                            </div>
+
+
+
                                             {{-- <p class="card-text"><small class="text-muted">{{ $hotel['propertyDetails']['address']['fullAddress'] }}</small></p> --}}
                                             <p class="card-text"><small class="text-muted"></small></p>
                                             <hr class="hr hr-blurry" />
                                             <div class="container">
-                                                  <span class="fa fa-star checked"></span>
-                                                  {{-- {{ $hotel['propertyDetails']['starRatingTitle'] }} --}}
+                                                <span class="fa fa-star checked"></span>
+                                                {{-- {{ $hotel['propertyDetails']['starRatingTitle'] }} --}}
                                             </div>
                                             <h6>Features:</h6>
                                             <p class="card-text">
                                                 {{ $item->description }}
-                                                {{-- @foreach($hotel['ameneties'][0]['content'] as $features)
+                                                {{-- @foreach ($hotel['ameneties'][0]['content'] as $features)
                                                     <button class="btn btn-outline-light text-dark shadow-none rounded-pill" style=" height: 30px;">{{ $features }}</button>
                                                 @endforeach --}}
-                                                <?php 
-                                                  //for($x = 0; $x < count())
+                                                <?php
+                                                //for($x = 0; $x < count())
                                                 ?>
                                             </p>
-                                                <a href="#" class="btn btn-light">Details</a>
-                                                <button class="btn btn-light">Edit</button>
+                                            <a href="#" class="btn btn-light">Details</a>
+                                            <button class="btn btn-light">Edit</button>
                                         </div>
                                     </div>
+                                </div>
+
+
                             </div>
-                            
-                              
-                        </div>
                         </div><!-- col ends -->
                         <?php $i++; ?>
-                        @empty
+                    @empty
                         No record Found
-                      @endforelse
-                        
-                    </div> <!-- row ends -->
+                    @endforelse
+
+                </div> <!-- row ends -->
             </div>
             <div class="d-flex justify-content-end">
-              {{ $data->links() }}
-          </div>
+                {{ $data->links() }}
+            </div>
         </section>
     </div>
 @endsection
