17c59165b18c26c099cc14e963607e2aaa611874
Neeraj Adhav <32135561+neerajadhav@users.noreply.github.com>
2023-03-03 16:09:10 +0530

    modified:   resources/views/subadmin/reviews/index.blade.php

--- resources/views/subadmin/reviews/index.blade.php
+++ resources/views/subadmin/reviews/index.blade.php
@@ -25,11 +25,11 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                            <h4 class="mb-sm-0">Reviews</h4>
+                            <h4 class="mb-sm-0">Reviews {{ session()->getId() }}</h4>
                             <div class="page-title-right">
                                 <ol class="breadcrumb m-0">
                                     <li class="breadcrumb-item">81 Support</li>
-                                    <li class="breadcrumb-item active">Reviews {{ Auth::id() }}</li>
+                                    <li class="breadcrumb-item active">Reviews</li>
                                 </ol>
                             </div>
                         </div>
