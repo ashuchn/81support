b947a1ad7bf0b56171022af9ab6bfc53880fda25
Neeraj Adhav <32135561+neerajadhav@users.noreply.github.com>
2023-03-03 14:57:07 +0530

    modified:   app/Http/Controllers/subadmin/ReviewController.php

--- app/Http/Controllers/subadmin/ReviewController.php
+++ app/Http/Controllers/subadmin/ReviewController.php
@@ -15,9 +15,6 @@ class ReviewController extends Controller
    public function getReview() {
         $id = Auth::user()->id;
         $reviews = DB::table('reviews')
-        ->join('products', 'reviews.productId', '=', 'products.id')
-        ->join('users', 'reviews.userId', '=', 'users.id')
-        ->select('reviews.*', 'products.name as product_name', 'users.name as user_name')
         ->where('reviews.userId', $id)
         ->get();
         return view('subadmin.reviews.index', compact('reviews'));
