372f56d99be0929b460ddf961898f5150a2151a3
Neeraj Adhav <32135561+neerajadhav@users.noreply.github.com>
2023-03-06 13:54:13 +0530

    modified:   resources/views/admin/app.blade.php

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -106,7 +106,7 @@
             var marker, i;
             for (i = 0; i < locations.length; i++) {
                 marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(locations[i][0], locations[i][1]),
+                    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
                     map: map
                 });
                 google.maps.event.addListener(marker, 'click', (function(marker, i) {
