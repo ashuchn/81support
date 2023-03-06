fcd3ff30ec712c3f786f1863fb2d1d5f1637aaff
Neeraj Adhav <32135561+neerajadhav@users.noreply.github.com>
2023-03-06 13:49:36 +0530

    modified:   app/Http/Controllers/Admin/AdminController.php 	modified:   resources/views/admin/app.blade.php

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -105,15 +105,15 @@
             var marker, i;
             for (i = 0; i < locations.length; i++) {
                 marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                    position: new google.maps.LatLng(locations[i][0], locations[i][1]),
                     map: map
                 });
-                google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                    return function() {
-                        infowindow.setContent(locations[i][0]);
-                        infowindow.open(map, marker);
-                    }
-                })(marker, i));
+                // google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                //     return function() {
+                //         infowindow.setContent(locations[i][0]);
+                //         infowindow.open(map, marker);
+                //     }
+                // })(marker, i));
             }
         }
         window.initMap = initMap;
