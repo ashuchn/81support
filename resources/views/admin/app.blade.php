498028fda50c9fa15792c718b78f8ab0bf457f7c
Neeraj Adhav <32135561+neerajadhav@users.noreply.github.com>
2023-03-06 14:12:07 +0530

    modified:   resources/views/admin/app.blade.php

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -90,35 +90,35 @@
         </div>
     </div>
 
-    {{-- <script type="text/javascript">
-        function initMap() {
-            const myLatLng = {
-                lat: 22.2734719,
-                lng: 70.7512559
-            };
-            const map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 5,
-                center: myLatLng,
-            });
-            var locations = {{ Js::from($locations) }};
-            console.log(locations);
-            var infowindow = new google.maps.InfoWindow();
-            var marker, i;
-            for (i = 0; i < locations.length; i++) {
-                marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-                    map: map
-                });
-                google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                    return function() {
-                        infowindow.setContent(locations[i][0]);
-                        infowindow.open(map, marker);
-                    }
-                })(marker, i));
-            }
-        }
-        window.initMap = initMap;
-    </script> --}}
+    <script type="text/javascript">
+        // function initMap() {
+        //     const myLatLng = {
+        //         lat: 22.2734719,
+        //         lng: 70.7512559
+        //     };
+        //     const map = new google.maps.Map(document.getElementById("map"), {
+        //         zoom: 5,
+        //         center: myLatLng,
+        //     });
+        //     var locations = {{ Js::from($locations) }};
+        //     console.log(locations);
+        //     var infowindow = new google.maps.InfoWindow();
+        //     var marker, i;
+        //     for (i = 0; i < locations.length; i++) {
+        //         marker = new google.maps.Marker({
+        //             position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        //             map: map
+        //         });
+        //         google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        //             return function() {
+        //                 infowindow.setContent(locations[i][0]);
+        //                 infowindow.open(map, marker);
+        //             }
+        //         })(marker, i));
+        //     }
+        // }
+        // window.initMap = initMap;
+    </script>
 
     <script type="text/javascript">
         function initializeMap() {
