c1c12db263a53b85619449869d0e19c7b03ce31d
Neeraj Adhav <32135561+neerajadhav@users.noreply.github.com>
2023-03-06 14:21:34 +0530

    modified:   app/Http/Controllers/Admin/AdminController.php 	modified:   resources/views/admin/app.blade.php

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -125,32 +125,6 @@
         window.initializeMap = initializeMap;
     </script>
 
-    <script type="text/javascript">
-        // function initializeMap() {
-        //     const locations = <?php echo json_encode($locations) ?>;
-    
-        //     const map = new google.maps.Map(document.getElementById("map"));
-        //     var infowindow = new google.maps.InfoWindow();
-        //     var bounds = new google.maps.LatLngBounds();
-        //     for (var location of locations) {
-        //         var marker = new google.maps.Marker({
-        //             position: new google.maps.LatLng(location.lat, location.lng),
-        //             map: map
-        //         });
-        //         bounds.extend(marker.position);
-        //         google.maps.event.addListener(marker, 'click', (function(marker, location) {
-        //             return function() {
-        //                 infowindow.setContent(location.lat + " & " + location.lng);
-        //                 infowindow.open(map, marker);
-        //             }
-        //         })(marker, location));
-    
-        //     }
-        //     map.fitBounds(bounds);
-        // }
-    </script>
-
-
     <script type="text/javascript"
         src="https://maps.google.com/maps/api/js?key={{ env('GOOGLE_MAPS_API') }}&callback=initializeMap"></script>
 @endsection
