e183b11c81d6f0bd5445a983c52be95b23be9bf4
Neeraj Adhav <32135561+neerajadhav@users.noreply.github.com>
2023-03-07 14:45:18 +0530

    modified:   app/Http/Controllers/Api/ShopController.php

--- app/Http/Controllers/Api/ShopController.php
+++ app/Http/Controllers/Api/ShopController.php
@@ -413,7 +413,7 @@ class ShopController extends Controller {
                     $dt->addedProduct = $product; 
                     unset($dt->userId);
                     unset($dt->productId);
-                    return array_values($dt->attributesToArray());
+                    return $dt;
                 } else {
                     return false;
                 }
