56892f2e02f1be32ac40ff9558fb7f2e1d0761d8
neerajadhav <adhavneeraj9500@gmail.com>
2023-03-22 17:05:02 +0530

    modified:   resources/views/admin/profile/index.blade.php

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -1,12 +1,62 @@
 @extends('admin.layout.layout')
 @section('css')
-    
 @endsection
 
 @section('content')
-    profile
+    <div class="content-wrapper">
+
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Riding Charter s</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Riding Charter </li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        @if (session('success'))
+            <div class="card-body">
+                <div class="alert alert-success alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <h5>{{ Session::get('success') }}</h5>
+                    <?php Session::forget('success'); ?>
+                </div>
+            </div>
+        @endif
+
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Profile</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                body
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+
+
+    </div>
 @endsection
 
 @section('script')
-    
 @endsection
