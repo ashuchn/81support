d17854b93dd834bc238648c53250d90a4cec469e
neerajadhav <adhavneeraj9500@gmail.com>
2023-03-22 17:48:12 +0530

    modified:   app/Http/Controllers/Admin/ProfileController.php

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -10,7 +10,7 @@ class ProfileController extends Controller
 {
     public function index(Request $request)
     {
-        $user_id = Auth::adminId();
+        $user_id = Session::get('adminId');
 
         return view('admin.profile.index' , compact('user_id'));
     }
