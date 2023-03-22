624681ddf9afcfc6978160c270e420a455714f17
neerajadhav <adhavneeraj9500@gmail.com>
2023-03-22 17:06:05 +0530

    modified:   resources/views/admin/profile/index.blade.php

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -9,12 +9,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Riding Charter s</h1>
+                        <h1>Profile Settings</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Riding Charter </li>
+                            <li class="breadcrumb-item active">Profile Settings</li>
                         </ol>
                     </div>
                 </div>
@@ -37,7 +37,7 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Profile</h3>
+                                <h3 class="card-title">Update Form</h3>
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body">
