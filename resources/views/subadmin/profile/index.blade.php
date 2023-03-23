70ae83dea616c50e32a65c700ac1f716ab94697a
neerajadhav <adhavneeraj9500@gmail.com>
2023-03-23 16:35:19 +0530

    modified:   resources/views/subadmin/partials/header.blade.php 	modified:   resources/views/subadmin/profile/index.blade.php

--- resources/views/subadmin/profile/index.blade.php
+++ resources/views/subadmin/profile/index.blade.php
@@ -1,1 +1,34 @@
-{{ $subadmin_id }}
+@extends('subadmin.layout.layout')
+@section('title','Dashboard | 81 Support')
+@section('content')
+
+<!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="main-content">
+
+                <div class="page-content">
+                    <div class="container-fluid">
+                        
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                    <h4 class="mb-sm-0">Profile</h4>
+
+                                    <div class="page-title-right">
+                                        <ol class="breadcrumb m-0">
+                                            <li class="breadcrumb-item">81 Support</li>
+                                            <li class="breadcrumb-item active"><a href="{{ route('subadmin.dashboard') }}">Profile</a></li>
+                                        </ol>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end page title -->
+
+                    </div>
+                </div>
+                <!-- End Page-content -->
+@endsection
