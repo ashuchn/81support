5d2505ab704c3b7297d04868d1eb113d9eae1d41
neerajadhav <adhavneeraj9500@gmail.com>
2023-03-24 12:24:35 +0530

    modified:   resources/views/subadmin/products/create.blade.php

--- resources/views/subadmin/products/create.blade.php
+++ resources/views/subadmin/products/create.blade.php
@@ -23,105 +23,103 @@
                     </div>
                 </div>
 
-                <form>
+                <form class="card">
                     <div class="row">
-                        <div class="card">
-                            <div class="col-lg-3">
-                                <div class="col-12">
-                                    <label for="exampleInputEmail1">Product Name</label>
-                                    <span class="text-danger">*</span>
-                                    <input type="text" name="productName"
-                                        class="form-control @error('productName') ? ' is-invalid' : '' @enderror"
-                                        placeholder="Enter Product Name" required>
-                                    @error('productName')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                                <div class="col-12">
-                                    <label for="price">Price (in $)</label>
-                                    <span class="text-danger">*</span>
-                                    <input type="text" name="price"
-                                        class="form-control @error('price') ? ' is-invalid' : '' @enderror"
-                                        placeholder="Enter Product Price" required>
-                                    @error('price')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
+                        <div class="col-lg-3">
+                            <div class="col-12">
+                                <label for="exampleInputEmail1">Product Name</label>
+                                <span class="text-danger">*</span>
+                                <input type="text" name="productName"
+                                    class="form-control @error('productName') ? ' is-invalid' : '' @enderror"
+                                    placeholder="Enter Product Name" required>
+                                @error('productName')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
                             </div>
-                            <div class="col-lg-9">
-                                <!-- Nav tabs -->
-                                <ul class="nav nav-tabs" role="tablist">
-                                    <li class="nav-item">
-                                        <a class="nav-link active" data-bs-toggle="tab" href="#home" role="tab">
-                                            <span class="d-block d-sm-none"><i class="fas fa-home"></i></span>
-                                            <span class="d-none d-sm-block">Home</span>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" data-bs-toggle="tab" href="#profile" role="tab">
-                                            <span class="d-block d-sm-none"><i class="far fa-user"></i></span>
-                                            <span class="d-none d-sm-block">Profile</span>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" data-bs-toggle="tab" href="#messages" role="tab">
-                                            <span class="d-block d-sm-none"><i class="far fa-envelope"></i></span>
-                                            <span class="d-none d-sm-block">Messages</span>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" data-bs-toggle="tab" href="#settings" role="tab">
-                                            <span class="d-block d-sm-none"><i class="fas fa-cog"></i></span>
-                                            <span class="d-none d-sm-block">Settings</span>
-                                        </a>
-                                    </li>
-                                </ul>
+                            <div class="col-12">
+                                <label for="price">Price (in $)</label>
+                                <span class="text-danger">*</span>
+                                <input type="text" name="price"
+                                    class="form-control @error('price') ? ' is-invalid' : '' @enderror"
+                                    placeholder="Enter Product Price" required>
+                                @error('price')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-lg-9">
+                            <!-- Nav tabs -->
+                            <ul class="nav nav-tabs" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" data-bs-toggle="tab" href="#home" role="tab">
+                                        <span class="d-block d-sm-none"><i class="fas fa-home"></i></span>
+                                        <span class="d-none d-sm-block">Home</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" data-bs-toggle="tab" href="#profile" role="tab">
+                                        <span class="d-block d-sm-none"><i class="far fa-user"></i></span>
+                                        <span class="d-none d-sm-block">Profile</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" data-bs-toggle="tab" href="#messages" role="tab">
+                                        <span class="d-block d-sm-none"><i class="far fa-envelope"></i></span>
+                                        <span class="d-none d-sm-block">Messages</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" data-bs-toggle="tab" href="#settings" role="tab">
+                                        <span class="d-block d-sm-none"><i class="fas fa-cog"></i></span>
+                                        <span class="d-none d-sm-block">Settings</span>
+                                    </a>
+                                </li>
+                            </ul>
 
-                                <!-- Tab panes -->
-                                <div class="tab-content p-3 text-muted">
-                                    <div class="tab-pane active" id="home" role="tabpanel">
-                                        <p class="mb-0">
-                                            Raw denim you probably haven't heard of them jean shorts Austin.
-                                            Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache
-                                            cliche tempor, williamsburg carles vegan helvetica. Reprehenderit
-                                            butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi,
-                                            qui irure terry richardson ex squid. Aliquip placeat salvia cillum
-                                            iphone. Seitan aliquip quis cardigan american apparel, butcher
-                                            voluptate nisi qui.
-                                        </p>
-                                    </div>
-                                    <div class="tab-pane" id="profile" role="tabpanel">
-                                        <p class="mb-0">
-                                            Food truck fixie locavore, accusamus mcsweeney's marfa nulla
-                                            single-origin coffee squid. Exercitation +1 labore velit, blog
-                                            sartorial PBR leggings next level wes anderson artisan four loko
-                                            farm-to-table craft beer twee. Qui photo booth letterpress,
-                                            commodo enim craft beer mlkshk aliquip jean shorts ullamco ad
-                                            vinyl cillum PBR. Homo nostrud organic, assumenda labore
-                                            aesthetic magna delectus.
-                                        </p>
-                                    </div>
-                                    <div class="tab-pane" id="messages" role="tabpanel">
-                                        <p class="mb-0">
-                                            Etsy mixtape wayfarers, ethical wes anderson tofu before they
-                                            sold out mcsweeney's organic lomo retro fanny pack lo-fi
-                                            farm-to-table readymade. Messenger bag gentrify pitchfork
-                                            tattooed craft beer, iphone skateboard locavore carles etsy
-                                            salvia banksy hoodie helvetica. DIY synth PBR banksy irony.
-                                            Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh
-                                            mi whatever gluten yr.
-                                        </p>
-                                    </div>
-                                    <div class="tab-pane" id="settings" role="tabpanel">
-                                        <p class="mb-0">
-                                            Trust fund seitan letterpress, keytar raw denim keffiyeh etsy
-                                            art party before they sold out master cleanse gluten-free squid
-                                            scenester freegan cosby sweater. Fanny pack portland seitan DIY,
-                                            art party locavore wolf cliche high life echo park Austin. Cred
-                                            vinyl keffiyeh DIY salvia PBR, banh mi before they sold out
-                                            farm-to-table VHS.
-                                        </p>
-                                    </div>
+                            <!-- Tab panes -->
+                            <div class="tab-content p-3 text-muted">
+                                <div class="tab-pane active" id="home" role="tabpanel">
+                                    <p class="mb-0">
+                                        Raw denim you probably haven't heard of them jean shorts Austin.
+                                        Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache
+                                        cliche tempor, williamsburg carles vegan helvetica. Reprehenderit
+                                        butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi,
+                                        qui irure terry richardson ex squid. Aliquip placeat salvia cillum
+                                        iphone. Seitan aliquip quis cardigan american apparel, butcher
+                                        voluptate nisi qui.
+                                    </p>
+                                </div>
+                                <div class="tab-pane" id="profile" role="tabpanel">
+                                    <p class="mb-0">
+                                        Food truck fixie locavore, accusamus mcsweeney's marfa nulla
+                                        single-origin coffee squid. Exercitation +1 labore velit, blog
+                                        sartorial PBR leggings next level wes anderson artisan four loko
+                                        farm-to-table craft beer twee. Qui photo booth letterpress,
+                                        commodo enim craft beer mlkshk aliquip jean shorts ullamco ad
+                                        vinyl cillum PBR. Homo nostrud organic, assumenda labore
+                                        aesthetic magna delectus.
+                                    </p>
+                                </div>
+                                <div class="tab-pane" id="messages" role="tabpanel">
+                                    <p class="mb-0">
+                                        Etsy mixtape wayfarers, ethical wes anderson tofu before they
+                                        sold out mcsweeney's organic lomo retro fanny pack lo-fi
+                                        farm-to-table readymade. Messenger bag gentrify pitchfork
+                                        tattooed craft beer, iphone skateboard locavore carles etsy
+                                        salvia banksy hoodie helvetica. DIY synth PBR banksy irony.
+                                        Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh
+                                        mi whatever gluten yr.
+                                    </p>
+                                </div>
+                                <div class="tab-pane" id="settings" role="tabpanel">
+                                    <p class="mb-0">
+                                        Trust fund seitan letterpress, keytar raw denim keffiyeh etsy
+                                        art party before they sold out master cleanse gluten-free squid
+                                        scenester freegan cosby sweater. Fanny pack portland seitan DIY,
+                                        art party locavore wolf cliche high life echo park Austin. Cred
+                                        vinyl keffiyeh DIY salvia PBR, banh mi before they sold out
+                                        farm-to-table VHS.
+                                    </p>
                                 </div>
                             </div>
                         </div>
