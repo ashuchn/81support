aec52c9b3dc31e005422ef5f6daf29b6fe9e7fb4
neerajadhav <adhavneeraj9500@gmail.com>
2023-03-24 14:31:27 +0530

    modified:   resources/views/subadmin/products/create.blade.php

--- resources/views/subadmin/products/create.blade.php
+++ resources/views/subadmin/products/create.blade.php
@@ -90,7 +90,7 @@
                                                         <input type="color" name="color" id="color"
                                                             placeholder="Enter Last Name"
                                                             class="form-control @error('color') is-invalid @enderror"
-                                                            value="{{ old('color') }}" style="height: 40px;">
+                                                            value="{{ old('color') }}" style="height: 40px; width: 30px;">
                                                         @error('color')
                                                             <p class="invalid-feedback">{{ $message }}</p>
                                                         @enderror
