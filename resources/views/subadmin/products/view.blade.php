608d0101ab4b38e08bfd5e2bf83c8128163f2f35
neerajadhav <adhavneeraj9500@gmail.com>
2023-03-26 18:01:47 +0530

    modified:   resources/views/subadmin/products/view.blade.php

--- resources/views/subadmin/products/view.blade.php
+++ resources/views/subadmin/products/view.blade.php
@@ -141,7 +141,7 @@
                                                 @foreach($quantities as $j => $qitem)
                                                     <tr>
                                                         <td>
-                                                            {{ $sizes-> }}
+                                                            {{ $sizes->size[$j] }}
                                                             <input name="sizes[]" type="text" value="{{ $sizes[$key] }}" class="d-none">
                                                         </td>
                                                         <td>
