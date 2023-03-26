26e931627cf86a0b565e44acfe1630fcd347aa08
neerajadhav <adhavneeraj9500@gmail.com>
2023-03-26 17:34:05 +0530

    modified:   resources/views/subadmin/products/create.blade.php

--- resources/views/subadmin/products/create.blade.php
+++ resources/views/subadmin/products/create.blade.php
@@ -83,7 +83,8 @@
                                 </div>
                                 <hr>
                                 <div id="ColorsSizeItem"></div>
-                                
+
+                                @foreach($colors as $color)
                                 <div class="row">
                                     <div class="col-lg-6">
                                         <table class="table">
@@ -232,6 +233,7 @@
                                         </table>
                                     </div>
                                 </div>
+                                @endforeach
                             </div>
                         </div>
                     </div>
