bb95b7f9747f7a9f9f4cabbe17409d334c79f583
neerajadhav <adhavneeraj9500@gmail.com>
2023-03-26 14:25:52 +0530

    modified:   resources/views/subadmin/products/create.blade.php

--- resources/views/subadmin/products/create.blade.php
+++ resources/views/subadmin/products/create.blade.php
@@ -93,7 +93,7 @@
                                                 <tr>
                                                     <td>
                                                         <div class="col-12">
-                                                            <select name="color" class="form-select select2" required>
+                                                            <select name="color[]" class="form-select select2" required>
                                                                 <option value="">Choose Color</option>
                                                                 <option value="1">Red</option>
                                                                 <option value="2">Blue</option>
@@ -361,7 +361,7 @@
                                     <tr>
                                         <td>
                                             <div class="col-12">
-                                                <select name="color" class="form-select select2" required>
+                                                <select name="color[]" class="form-select select2" required>
                                                     <option value="">Choose Color</option>
                                                     <option value="1">Red</option>
                                                     <option value="2">Blue</option>
