9cb4c40ff7714bc66596b7a597c3ab1f7ff8a641
ashuchn <ashutoshchauhan129@gmail.com>
2023-04-02 20:40:12 +0530

    ui changes

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -84,11 +84,27 @@
                 </div>
                 <!-- /.row -->
                 <div class="row">
-                    <div class="col-md-12 col-lg-6">
+                    <div class="col-md-12 col-lg-6 ">
                         <div class="card">
                             <div class="card-header text-white" style="background-color: #6610f2"><h4>Our Riding Charters</h4></div>
                             <div class="card-body">
                                 <div id="map" style="height: 500px; width: 100%;"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-lg-6  ">
+                        <div class="card ">
+                            <div class="card-header text-white" style="background-color: #6610f2">
+                                <div class="col-md-6">
+                                    <h4>Sales in February</h4>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="float-md-end d-flex align-items-center">Helllo</div>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <canvas id="myChart" style="height: 500px; width: 100%;"></canvas>
+
                             </div>
                         </div>
                     </div>
@@ -97,41 +113,76 @@
         </div>
     </div>
 
-    <script type="text/javascript">
-        function initializeMap() {
-            const myLatLng = {
-                lat: 22.2734719,
-                lng: 70.7512559
-            };
-            const map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 5,
-                center: myLatLng,
-            });
-            var locations = {{ Js::from($locations) }};
-            var infowindow = new google.maps.InfoWindow();
-            var marker, i;
-            var bounds = new google.maps.LatLngBounds();
-            for (var location of locations) {
-                var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(location.latitude, location.longitude),
-                    map: map
-                });
-                bounds.extend(marker.position);
-                google.maps.event.addListener(marker, 'click', (function(marker, location) {
-                    return function() {
-                        infowindow.setContent(location.name);
-                        infowindow.open(map, marker);
-                    }
-                })(marker, location));
     
-            }
-            map.fitBounds(bounds);
-        }
-        window.initializeMap = initializeMap;
-    </script>
+@endsection
 
-    <script type="text/javascript"
-    src="https://maps.google.com/maps/api/js?key={{ config('app.GOOGLE_MAPS_API') }}&callback=initializeMap"></script>
-        {{-- src="https://maps.google.com/maps/api/js?key={{ env('GOOGLE_MAPS_API') }}&callback=initializeMap"></script> --}}
+@section('script')
+<script type="text/javascript">
+    function initializeMap() {
+        const myLatLng = {
+            lat: 22.2734719,
+            lng: 70.7512559
+        };
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 5,
+            center: myLatLng,
+        });
+        var locations = {{ Js::from($locations) }};
+        var infowindow = new google.maps.InfoWindow();
+        var marker, i;
+        var bounds = new google.maps.LatLngBounds();
+        for (var location of locations) {
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(location.latitude, location.longitude),
+                map: map
+            });
+            bounds.extend(marker.position);
+            google.maps.event.addListener(marker, 'click', (function(marker, location) {
+                return function() {
+                    infowindow.setContent(location.name);
+                    infowindow.open(map, marker);
+                }
+            })(marker, location));
+
+        }
+        map.fitBounds(bounds);
+    }
+    window.initializeMap = initializeMap;
+</script>
+
+<script type="text/javascript"
+src="https://maps.google.com/maps/api/js?key={{ config('app.GOOGLE_MAPS_API') }}&callback=initializeMap"></script>
+    {{-- src="https://maps.google.com/maps/api/js?key={{ env('GOOGLE_MAPS_API') }}&callback=initializeMap"></script> --}}
+
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script type="text/javascript">
+  
+    var labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        // data: users,
+        data: [12, 19, 3, 5, 2, 3],
+      }]
+    };
+
+    const config = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+
+    const myChart = new Chart(
+      document.getElementById('myChart'),
+      config
+    );
+
+</script>
 
 @endsection
--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -40,11 +40,12 @@
                     <!-- /.card-header -->
                     <div class="card-body">
                         <table class="table table-bordered table-striped table-responsive-sm">
-                            <thead>
+                            <thead style="background-color:#B9D9EB" class="">
                                 <tr>
                                     <th>Image</th>
                                     <th>Name</th>
                                     <th>Features</th>
+                                    <th>Stock Count</th>
                                     <th>Rating</th>
                                     <th>Actions</th>
                                 </tr>
@@ -62,14 +63,21 @@
                                         </td>
                                         <td>{{ $item->productName }}</td>
                                         <td>{{ $item->description }}</td>
+                                        <td>{{ $item->available_quantity }}</td>
                                         <td>
                                             <div class="container">
                                                 <span class="fa fa-star checked"></span>
                                             </div>
                                         </td>
-                                        <td>
-                                            <a href="#" class="btn btn-light">Details</a>
-                                            <button class="btn btn-light">Edit</button>
+                                        <td class="d-flex">
+                                            {{-- <a href="#" class="btn btn-primary">Details</a> --}}
+                                            <button href="#" type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Details">
+                                                <i class="fa-solid fa-circle-info"></i>
+                                            </button>
+                                            {{-- <button class="btn" style="background-color: #01ff70">Edit</button> --}}
+                                            <button class="btn" type="button" style="background-color: #01ff70" data-toggle="tooltip" data-placement="top" title="Edit">
+                                                <i class="fa-solid fa-pen-to-square"></i>
+                                            </button>
                                         </td>
                                     </tr>
                                     <?php $i++; ?>
@@ -82,6 +90,7 @@
                                     <th>Image</th>
                                     <th>Name</th>
                                     <th>Features</th>
+                                    <th>Stock Count</th>
                                     <th>Rating</th>
                                     <th>Actions</th>
                                 </tr>
