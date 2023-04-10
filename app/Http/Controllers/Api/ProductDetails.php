fc75078d1b8c66644067d3f706fd2e75a97dce2d
neerajadhav <adhavneeraj9500@gmail.com>
2023-04-10 15:58:54 +0530

    new file:   app/Http/Controllers/Api/ProductDetails.php 	modified:   routes/api.php

---
+++ app/Http/Controllers/Api/ProductDetails.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Notification;
+use Hash;
+use DB;
+
+class ProductDetails extends Controller {
+    
+    public function getProductDetails(Request $req)
+    {
+        $valid = Validator::make($req->all(),[
+            "productId" => "Required|exists:products,id",
+        ],[
+            "productId.required" => "Provide Product Id",
+            "productId.exists"   => "Product Id not Found"
+        ]);
+
+        if($valid->fails()) {
+            return response()->json([
+                "response_message" => $valid->errors()->first(),
+                "response_code"    => 401,
+            ],401);
+        }
+        
+        $data = Product::find($req->productId);
+        $totalRatings = DB::table('reviews')->where('productId',$data->id)->count();
+        if($totalRatings > 0) {
+            $ratings = DB::table('reviews')->where('productId',$data->id)->sum('rating');
+            $avgRating = $ratings/$totalRatings;
+        } else {
+            $avgRating = 0;
+        }
+        $data->avgRating = $avgRating;
+        $images = DB::table('product_images')->where('productId',$data->id)->pluck('image');
+        if(isset($images)) {
+            $img = $images->map(function($im){
+                return url('/').'/'.$im; 
+            });
+        }
+        $data->image = $img;
+        
+        $reviews = DB::table('reviews')->where('productId',$data->id)->get(['id','userId','productId','rating','description']);
+        if(isset($reviews)) {
+            $review = $reviews->map(function($rv){
+                 $user = New_User::find($rv->userId);
+                 if($user) {
+                    $rv->userName = $user->name;
+                    $rv->userImage = $user->image;
+                 } else {
+                    $rv->userName = null;
+                    $rv->userImage = null;
+                 }
+                 return $rv;
+            });
+        } else {
+            $review = [];
+        }
+        
+        $data->reviews = $review;
+        return response()->json([
+            "response_message" => "Ok!",
+            "response_code"    => 200,
+            "data"             => $data
+        ],200);   
+    }
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\ShopController;
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\NotificationController;
 use App\Http\Controllers\Api\ReviewController;
 use App\Mail\sendOtp;
@@ -41,6 +42,7 @@ Route::group(['middleware' => 'auth:sanctum'],function () {
     Route::post('bookmarkProduct', [ShopController::class, 'bookmarkProduct']);
     Route::delete('deleteBookmarkedProduct/{id}',[ShopController::class, 'deleteBookmarkedProduct']);    
     Route::get('getProduct',[ShopController::class,'getProduct']);
+    Route::get('getProductDetails',[ProductController::class,'getProductDetails']);
     Route::get('getCart',[ShopController::class,'getCart']);
     Route::get('getBookmarks',[ShopController::class,'getBookmarks']);
     Route::post('getDeals',[ShopController::class,'getDeals']);
