c48cc5fdd47fb77a958828c537627c5b4e07598a
neerajadhav <adhavneeraj9500@gmail.com>
2023-04-17 15:42:24 +0530

    modified:   app/Http/Controllers/Api/ProductDetails.php

--- app/Http/Controllers/Api/ProductDetails.php
+++ app/Http/Controllers/Api/ProductDetails.php
@@ -133,7 +133,7 @@ class ProductDetails extends Controller
             //     $data->sizes = $size;
             // }
             foreach($sizes as $key => $value){
-                $sizes[$key] = DB::table('sizes')->where('id', $value->size)->first()->size;
+                $sizes[$key][$value] = ProductSizeQuantity::where('product_id', $productId)->where('color', $current_color)->where('size', $value->size)->first()->quantity;
             }
             $data->sizes = $sizes;
         }else{
